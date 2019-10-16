b2c420eae16c24571178202482e63d00c8555458
MikeyBeez <mbonsign@gmail.com>
2019-10-16 17:19:47 -0400

    Initial commit

---
+++ crap.py
@@ -0,0 +1,20 @@
+import math
+import sys
+from os import rename
+
+import requests
+
+# print(sys.version)
+# print(sys.executable)
+
+ 
+print("Hello, Michael")  
+
+def greet(who_to_greet):
+    # test = 'foo'
+    greeting = "HelloM. {}".format(who_to_greet)
+    return greeting
+
+
+r = requests.get("https://coreyms.com")
+print(r.status_code)
